7564020b9a31c02ff15d537fe426c24aabf5d12d
librehat <hzwhuang@gmail.com>
2012-12-07 21:34:41 +0800

    Deleting 05wifi script.

--- w22pro/full_w22.mk
+++ w22pro/full_w22.mk
@@ -33,7 +33,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.d/01cpu:system/etc/init.d/01cpu \
     $(LOCAL_PATH)/init.d/02modules:system/etc/init.d/02modules \
     $(LOCAL_PATH)/init.d/03kernel:system/etc/init.d/03kernel \
-    $(LOCAL_PATH)/init.d/05wifi:system/etc/init.d/05wifi \
     $(LOCAL_PATH)/init.d/06fixpermissions:system/etc/init.d/06fixpermissions \
     $(LOCAL_PATH)/init.d/10devlistener:system/etc/init.d/10devlistener \
     $(LOCAL_PATH)/init.d/11link2sd:system/etc/init.d/11link2sd
