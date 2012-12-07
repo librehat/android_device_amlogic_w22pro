cbc13bab1a8523c9bcef1e941fac728627fdc62a
librehat <hzwhuang@gmail.com>
2012-12-07 12:37:45 +0800

    Clean and fix.

--- w22pro/full_w22.mk
+++ w22pro/full_w22.mk
@@ -34,7 +34,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.d/02modules:system/etc/init.d/02modules \
     $(LOCAL_PATH)/init.d/03kernel:system/etc/init.d/03kernel \
     $(LOCAL_PATH)/init.d/05wifi:system/etc/init.d/05wifi \
-    $(LOCAL_PATH)/init.d/10devlistener:system/etc/init.d/10devlistener
+    $(LOCAL_PATH)/init.d/06fixpermissions:system/etc/init.d/06fixpermissions \
+    $(LOCAL_PATH)/init.d/10devlistener:system/etc/init.d/10devlistener \
     $(LOCAL_PATH)/init.d/11link2sd:system/etc/init.d/11link2sd
 
 # Prebuilt configs
@@ -52,7 +53,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/set_display_mode.sh:system/bin/set_display_mode.sh \
     $(LOCAL_PATH)/configs/Vendor_0001_Product_0001.kl:/system/usr/keylayout/Vendor_0001_Product_0001.kl \
     $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab \
-    $(LOCAL_PATH)/configs/wififix.sh:system/bin/wififix.sh
+    $(LOCAL_PATH)/configs/wififix.sh:system/bin/wififix.sh \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/mcli-cdma:system/etc/ppp/peers/mcli-cdma \
+    $(LOCAL_PATH)/configs/mcli-gsm:system/etc/ppp/peers/mcli-gsm
 
 # USB ModeSwitch
 PRODUCT_COPY_FILES += \
@@ -73,27 +78,29 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/battery/3.bmp:system/resource/battery_pic/3.bmp \
     $(LOCAL_PATH)/proprietary/battery/4.bmp:system/resource/battery_pic/4.bmp \
     $(LOCAL_PATH)/proprietary/battery/power_low.bmp:system/resource/battery_pic/power_low.bmp \
+    $(LOCAL_PATH)/proprietary/vold/fsck.exfat:system/bin/fsck.exfat \
+    $(LOCAL_PATH)/proprietary/vold/mkfs.exfat:system/bin/mkfs.exfat \
+    $(LOCAL_PATH)/proprietary/vold/mount.exfat:system/bin/mount.exfat \
     $(LOCAL_PATH)/proprietary/vold/ntfs-3g:system/bin/ntfs-3g \
     $(LOCAL_PATH)/proprietary/vold/ntfs-3g.probe:system/bin/ntfs-3g.probe \
     $(LOCAL_PATH)/proprietary/vold/mkntfs:system/bin/mkntfs \
-    $(LOCAL_PATH)/proprietary/fbset:system/xbin/fbset \
+    $(LOCAL_PATH)/proprietary/fbset:system/xbin/fbset
 
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-    frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
-    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/base/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 DEVICE_PACKAGE_OVERLAYS := device/amlogic/w22pro/overlay
@@ -101,9 +108,10 @@ DEVICE_PACKAGE_OVERLAYS := device/amlogic/w22pro/overlay
 # AML HAL
 PRODUCT_PACKAGES += \
     camera.amlogic.so \
+    hwcomposer.amlogic \
     sensors.amlogic \
     lights.amlogic \
-    hwcomposer.amlogic
+    power.amlogic
 
 # Mali GPU OpenGL libraries
 PRODUCT_PACKAGES += \
@@ -132,6 +140,9 @@ PRODUCT_PACKAGES += \
     init-pppd.sh \
     ip-up \
     chat
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/libril/rild:system/bin/rild
 
 # ALSA
 PRODUCT_PACKAGES += \
@@ -190,26 +201,24 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.enable_bypass=1 \
     dalvik.vm.execution-mode=int:jit \
     dalvik.vm.lockprof.threshold=500 \
-    dalvik.vm.dexopt-flags=m=y \
+    dalvik.vm.verify-bytecode=false \
+    dalvik.vm.dexopt-flags=v=n,o=v \
     dalvik.vm.dexopt-data-only=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.timezone=Asia/Shanghai \
     persist.sys.language=zh \
     persist.sys.country=CN \
-    ro.com.android.dateformat=yyyy-MM-dd \
-    ro.product.locale.language=zh \
-    ro.product.locale.region=CN \
     persist.sys.use_dithering=0 \
     persist.sys.purgeable_assets=0 \
     windowsmgr.max_events_per_sec=240 \
     view.touch_slop=2 \
     view.minimum_fling_velocity=25 \
-    ro.additionalmounts=/mnt/external_sdcard \
-    ro.vold.switchablepair=/mnt/sdcard,/mnt/external_sdcard \
+    ro.additionalmounts=/storage/sdcard1 \
+    ro.vold.switchablepair=/storage/sdcard0,/storage/sdcard1 \
     persist.sys.vold.switchexternal=0
    
-PRODUCT_AAPT_CONFIG := large mdpi
+PRODUCT_AAPT_CONFIG := normal large tvdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 
 PRODUCT_CHARACTERISTICS := tablet
@@ -220,6 +229,6 @@ TARGET_BOOTANIMATION_NAME := horizontal-1024x768
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-$(call inherit-product, frameworks/base/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 $(call inherit-product, build/target/product/full_base.mk)
 $(call inherit-product-if-exists, vendor/amlogic/w22pro/w22-vendor.mk)
